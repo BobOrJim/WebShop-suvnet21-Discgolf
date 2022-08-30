15e1a1b4ec887bc5b7c73c89b510a9556e8d6a19
Frans Billquist <frans@Franss-MacBook-Pro.local>
2022-08-30 22:32:36 +0200

    added link via Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./css/app.css";
+import {Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,14 @@ function App() {
   return (
     <div className="App">
       <h1>Frissbee-discogolf</h1>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<adminPage />}/>
+      <Route path="/" element={<checkoutPage />}/>
+      <Route path="/" element={<detailsProductPage />}/>
+      <Route path="/" element={<productPage />}/>
+      
+    </Routes>
     </div>
   );
 }
