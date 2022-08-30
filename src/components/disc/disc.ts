73e9fecfdca15042e5c30a6820770d47adfaa6ea
Frans Billquist <frans@Franss-MacBook-Pro.local>
2022-08-30 21:57:32 +0200

    fixed typ errors

--- src/components/disc/disc.ts
+++ src/components/disc/disc.ts
@@ -2,16 +2,16 @@ export { };
 
 //interface
 interface Disc {
-    id: "string",
-    name: "string",
-    brand: "string",
-    speed: "number",
-    glide: "number",
-    turn: "number",
-    fade: "number",
-    weight: "number",
-    color: "string",
-    imageUrl: "string"
+    id: string,
+    name: string,
+    brand: string,
+    speed: number,
+    glide: number,
+    turn: number,
+    fade: number,
+    weight: number,
+    color: string,
+    imageUrl: string
 }
 
 //seedning
