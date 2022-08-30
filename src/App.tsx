6ecb8f45bd61edf55bac6deece86331b120baa29
Frans Billqvist <frans.billquist@gmail.com>
2022-08-30 23:33:29 +0200

    added Rout and routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import "./css/app.css";
-import {Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import AdminPage from "./pages/AdminPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import DetailedProductPage from "./pages/DetailedProductPage";
+import ProductsPage from "./pages/ProductsPage";
+import { Container } from "@mui/material";
+import {AppBar} from "@mui/material";
+
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div>
       <h1>Frissbee-discogolf</h1>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/" element={<adminPage />}/>
-      <Route path="/" element={<checkoutPage />}/>
-      <Route path="/" element={<detailsProductPage />}/>
-      <Route path="/" element={<productPage />}/>
-      
-    </Routes>
-    </div>
-  );
-}
+    <AppBar> 
+      <Container>
 
+        <Routes>
+
+          <Route path="/" element={<Home />}/>
+          <Route path="/AdminPage" element={<AdminPage />}/>
+          <Route path="/CheckoutPage" element={<CheckoutPage />}/>
+          <Route path="/DetailedProductPage" element={<DetailedProductPage />}/>
+          <Route path="/ProductsPage" element={<ProductsPage />}/>
+      
+        </Routes>
+      </Container>
+    </AppBar>
+    </div>
+  )
+}
 export default App;
