8b5ab2b848e98c29c707cc1decba9922d2079b37
Frans Billqvist <frans.billquist@gmail.com>
2022-09-01 12:42:26 +0200

    Added Grid2 in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Grid2 from "@mui/material/Unstable_Grid2";
 import { CSSProperties, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { seedData } from "../components/disc/disc";
@@ -5,11 +6,13 @@ import { DiscCard } from "../components/disc/DiscCard";
 
 const Home = () => {
   return (
-    <div>
+    <Grid2 container flexGrow={1} rowSpacing={4}  columnSpacing={{ xs:2 , md:2}}>
       {seedData.map((disc) => (
+        <Grid2 xs={4} md={4} lg={4}>
         <DiscCard key={disc.id} disc={disc} />
+        </Grid2>
       ))}
-    </div>
+    </Grid2>
   );
 };
 
