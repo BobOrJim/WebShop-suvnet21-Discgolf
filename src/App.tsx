c7e1fc9cc05673afb1498cf6587ffe90c3bf09aa
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2022-09-03 00:58:48 +0200

    admin table

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import AdminPage from "./pages/AdminPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import DetailedProductPage from "./pages/DetailedProductPage";
 import Home from "./pages/Home";
+import EditPage from "./pages/EditPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path=":detailedproductpage" element={<DetailedProductPage />}>
+        <Route path="detailedproductpage" element={<DetailedProductPage />}>
           <Route path=":discId" element={<DetailedProductPage />} />
         </Route>
         <Route path="adminpage" element={<AdminPage />} />
         <Route path="checkoutpage" element={<CheckoutPage />} />
+      </Route>
+      <Route path="editpage" element={<EditPage />}>
+        <Route path=":discId" element={<EditPage />} />
       </Route>
       <Route path="*" element={<div>404 page does not exist</div>}></Route>
     </Routes>
