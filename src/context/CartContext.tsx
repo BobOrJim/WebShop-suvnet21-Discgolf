97466b6377589e4f8574c1c3c37bc07198bf2101
LucasB <lucas_benjaminson@hotmail.com>
2022-09-06 19:49:15 +0200

    Fixed product.ts name and all tsc problems

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -33,7 +39,10 @@ export function CartContextProvider({ children }: CartProviderProps) {
     localStorage.setItem("products", JSON.stringify(cartItems));
   }, [cartItems]);
   const [isOpen, setIsOpen] = useState(false);
-  const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0);
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
 
   const openCart = () => setIsOpen(true);
   const closeCart = () => setIsOpen(false);
