cfcbb6ca8d8945bb1392019c80575b5b8b79ef0d
LucasB <lucas_benjaminson@hotmail.com>
2022-09-09 11:27:48 +0200

    validation added

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
+import CssBaseline from "@mui/material/CssBaseline";
 import Paper from "@mui/material/Paper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
@@ -14,31 +14,18 @@ import { linkStyle } from "../../Layout";
 import AddressForm from "./AddressForm";
 import Review from "./Review";
 
-function Copyright() {
-  return (
-    <Typography variant='body2' color='text.secondary' align='center'>
-      {"Copyright © "}
-      <Link color='inherit' href='https://mui.com/'>
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 const steps = ["Shipping address", "Review your order"];
 
-function getStepContent(step: number) {
-  switch (step) {
-    case 0:
-      return <AddressForm />;
-    case 1:
-      return <Review />;
-    default:
-      throw new Error("Unknown step");
-  }
-}
+// function getStepContent(step: number) {
+//   switch (step) {
+//     case 0:
+//       return <Review />;
+//     case 1:
+//       return <Review />;
+//     default:
+//       throw new Error("Unknown step");
+//   }
+// }
 
 const theme = createTheme();
 
@@ -55,6 +42,8 @@ export default function Checkout() {
 
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
+
       <Container component='main' maxWidth='sm' sx={{ mb: 4 }}>
         <Paper variant='outlined' sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component='h1' variant='h4' align='center'>
@@ -79,22 +68,20 @@ export default function Checkout() {
               </React.Fragment>
             ) : (
               <React.Fragment>
-                {getStepContent(activeStep)}
+                {/* {getStepContent(activeStep)} */}
                 <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
                   {activeStep !== 0 && (
                     <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
                       Back
                     </Button>
                   )}
-                  <Button variant='contained' onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
-                    {activeStep === steps.length - 1 ? "Place order" : "Next"}
-                  </Button>
                 </Box>
+                {activeStep === 0 && <AddressForm submit={handleNext} />}
+                {activeStep === 1 && <Review submit={handleNext} />}
               </React.Fragment>
             )}
           </React.Fragment>
         </Paper>
-        <Copyright />
       </Container>
     </ThemeProvider>
   );
