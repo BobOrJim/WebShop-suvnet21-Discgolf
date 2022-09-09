6c5d4761a9947c69e4cc30f3310ad965e5143595
LucasB <lucas_benjaminson@hotmail.com>
2022-09-09 14:41:59 +0200

    hotfix

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -8,7 +8,7 @@ import StepLabel from "@mui/material/StepLabel";
 import Stepper from "@mui/material/Stepper";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import as React from "react";
+import * as React from "react";
 import { NavLink } from "react-router-dom";
 import { linkStyle } from "../../Layout";
 import AddressForm from "./AddressForm";
