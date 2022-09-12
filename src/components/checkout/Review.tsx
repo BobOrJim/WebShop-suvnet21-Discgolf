fa1c94504e20cf9e0617dd0eb97f5ba6887b2f34
LucasB <lucas_benjaminson@hotmail.com>
2022-09-12 21:09:07 +0200

    WIP

--- src/components/checkout/Review.tsx
+++ src/components/checkout/Review.tsx
@@ -1,52 +1,74 @@
-import { Button } from "@mui/material";
+import { Button, List } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
-import * as React from "react";
 import { useCartContext } from "../../context/CartContext";
 import { useProductContext } from "../../context/ProductContext";
-import { CartItem } from "../CartItem";
-import { Product } from "../product/product";
 
 interface Props {
   submit: () => void;
 }
 
+type CheckoutItem = {
+  name: string;
+  price: number;
+  amount: number;
+  imageUrl: string;
+};
+
 export default function Review({ submit }: Props) {
   const { cartItems } = useCartContext();
   const { getAllProducts } = useProductContext();
-  const [products, setProducts] = React.useState<Product[]>([]);
+  // const [products, setProducts] = useState<Product[]>([]);
+  const checkoutItems: CheckoutItem[] = [];
 
+  function createCheckoutItem() {
+    const newCheckoutItem: CheckoutItem = {
+      name: "",
+      price: 0,
+      amount: 0,
+      imageUrl: "",
+    };
+    cartItems.map((item) => {
+      newCheckoutItem.amount = item.quantity;
+      const product = getAllProducts().find((i) => i.id === item.id);
+      if (product) {
+        newCheckoutItem.name = product.name;
+        newCheckoutItem.price = product.price;
+        newCheckoutItem.imageUrl = product.imageUrl;
+      }
+      checkoutItems.push(newCheckoutItem);
+    });
+  }
+  createCheckoutItem();
   return (
-    <React.Fragment>
+    <>
       <Typography variant='h6' gutterBottom>
         Order summary
       </Typography>
-      <List disablePadding>
-        <>
-          {cartItems.map((item) => {
-            <CartItem key={item.id} {...item} />;
-            const product = getAllProducts().find((i) => i.id === item.id);
-            const addProducts = (product: Product) => {
-              setProducts((prevState) => [...prevState, product]);
-            };
-          })}
-          <ListItem sx={{ py: 1, px: 0 }}>
-            <ListItemText primary='test' secondary='test 2' />
-            <Typography variant='body2'>test 3</Typography>
 
-            <ListItemText primary='Total' />
-            <Typography variant='subtitle1' sx={{ fontWeight: 700 }}></Typography>
-          </ListItem>
-        </>
-      </List>
+      {checkoutItems.map((item, index) => {
+        <div key={index}>{item.name}</div>;
+      })}
+
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <Typography variant='h6' gutterBottom sx={{ mt: 2 }}>
             Shipping
           </Typography>
+          <List>
+            {/* {checkoutItems.map((item, index) => {
+              <ListItem key={index} sx={{ py: 1, px: 0 }}>
+                {item.name}
+              </ListItem>;
+            })} */}
+          </List>
+          {/* <ListItem sx={{ py: 1, px: 0 }}>
+            <ListItemText primary='test' secondary='test 2' />
+
+            <Typography variant='body2'></Typography>
+            <ListItemText primary='Total' />
+            <Typography variant='subtitle1' sx={{ fontWeight: 700 }}></Typography>
+          </ListItem>
           <Typography gutterBottom>John Smith</Typography>
           {/* <Typography gutterBottom>{addresses.join(", ")}</Typography> */}
         </Grid>
@@ -54,6 +76,6 @@ export default function Review({ submit }: Props) {
       <Button type='submit' onClick={submit}>
         Confirm
       </Button>
-    </React.Fragment>
+    </>
   );
 }
