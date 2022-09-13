bc4a696c58ecae0a6c4cfa1633fd3c60d7628876
LucasB <lucas_benjaminson@hotmail.com>
2022-09-13 19:56:53 +0200

    Changed some styling and removed third scrollbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,10 +6,10 @@ import { useProductContext } from "../context/ProductContext";
 const Home = () => {
   const { getAllProducts } = useProductContext();
   return (
-    <Box sx={{ width: "100%", overflowX: "auto", marginTop: "75px" }}>
-      <Grid2 container flexGrow={1} rowSpacing={4} columnSpacing={{ xs: 2, md: 2 }}>
+    <Box sx={{ width: "100%", marginTop: "5rem" }}>
+      <Grid2 container flexGrow={1} rowSpacing={1} columnSpacing={{ xs: 12, md: 8 }}>
         {getAllProducts().map((item) => (
-          <Grid2 key={item.id} xs={4} md={4} lg={4}>
+          <Grid2 key={item.id} xs={12} md={8} lg={4}>
             <ProductCard {...item} />
           </Grid2>
         ))}
