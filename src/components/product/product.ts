5e79d22b48d3906d82ed8c37bcacfe8bc899bb5f
Daniel T <90195897+DTSD21@users.noreply.github.com>
2022-09-14 20:08:06 +0200

    make changes to deleted files to bring em back

--- src/components/product/product.ts
+++ src/components/product/product.ts
@@ -1,13 +1,26 @@
+<<<<<<< HEAD
 import { ProductCategory } from "./ProductCategory";
+=======
+>>>>>>> ed1bfb2 (make changes to deleted files to bring em back)
 export type Product = {
   id: string;
   name: string;
   brand: string;
+<<<<<<< HEAD
+=======
+  speed: number;
+  glide: number;
+  turn: number;
+  fade: number;
+>>>>>>> ed1bfb2 (make changes to deleted files to bring em back)
   weight: number;
   color: string;
   imageUrl: string;
   price: number;
+<<<<<<< HEAD
   category: ProductCategory;
+=======
+>>>>>>> ed1bfb2 (make changes to deleted files to bring em back)
   type: string;
 };
 
